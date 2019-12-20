f3696d7419e1c11631da11b5950021b13bb1b6d9
YeonwooSung <neos960518@gmail.com>
2019-12-20 23:46:20 +0900

    Finish the practical

--- A4src/A4main.py
+++ A4src/A4main.py
@@ -1,6 +1,5 @@
 import sys
 from warnings import simplefilter
-from os import path
 
 import part1
 import part2
@@ -25,13 +24,8 @@ if __name__ == "__main__":
             print("Executing Intermediate Agent!")
 
             while True:
-                # check if the "newTickets.csv" file exists
-                if path.exists('./newTickets.csv'):
-                    print('Use newTickets.csv')
-                    part2.ask_user(file_to_use='./newTickets.csv')
-                else:
-                    print('Use newTickets.csv')
-                    part2.ask_user()
+                part2.ask_user()
+                print("\n\nNow let's handle next ticket!")
         elif (agentName == 'Adv'):
             print("Executing Advanced Agent!")
             print("Using file : tickets.csv")
